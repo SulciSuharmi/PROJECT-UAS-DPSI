1477dd9ab2cd5c9c0c5af006996339efdf574883
SulciSuharmi <167896490+SulciSuharmi@users.noreply.github.com>
2024-07-24 13:39:07 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,22 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config(); // Make sure to load environment variables
+const jwt = require('jsonwebtoken'); // Library untuk membuat dan memverifikasi token JWT
+require('dotenv').config(); // Library untuk memuat variabel lingkungan dari file .env
 
-// Middleware untuk memverifikasi token JWT dan mengatur data pengguna ke dalam objek request
+// Middleware untuk memverifikasi token JWT dan menyimpan informasi pengguna yang terverifikasi ke dalam objek request
 exports.authenticateToken = (req, res, next) => {
   // Mengambil token dari header Authorization, yang biasanya dalam format "Bearer <token>"
   const token = req.header('Authorization')?.split(' ')[1];
   console.log("Extracted token:", token);
+
   // Jika tidak ada token, kirimkan status 401 (Unauthorized)
   if (!token) return res.status(401).json({ message: 'Access denied' });
 
   try {
     // Verifikasi token menggunakan secret key
     const verified = jwt.verify(token, process.env.JWT_SECRET);
+
     // Menyimpan informasi pengguna yang terverifikasi ke dalam objek request
     req.user = verified;
+
     // Melanjutkan ke middleware berikutnya
     next();
   } catch (error) {
@@ -36,14 +39,15 @@ exports.authorizeRole = (role) => {
   };
 };
 
+// Middleware untuk memeriksa apakah pengguna adalah admin atau username yang diautentikasi sesuai dengan username di parameter URL
 exports.authorizeUsername = (req, res, next) => {
   const { username } = req.params;
 
-  // Check if user's role is 'admin' or if the username matches the authenticated user's username
+  // Memeriksa apakah peran pengguna adalah 'admin' atau username sesuai dengan username di parameter URL
   if (req.user.role !== 'admin' && req.user.username !== username) {
-    // If not, send 403 (Forbidden)
+    // Jika tidak, kirimkan status 403 (Forbidden)
     return res.status(403).json({ message: 'Forbidden' });
   }
-  // Proceed to the next middleware if the username matches
+  // Melanjutkan ke middleware berikutnya jika username sesuai
   next();
 };
