6bcb4380415220a1b00380a5b0148faba884cac7
SulciSuharmi <167896490+SulciSuharmi@users.noreply.github.com>
2024-07-24 13:39:33 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sql12721109', 'sql12721109', 'MunU71qVE5', {
-    host: 'sql12.freesqldatabase.com',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectModule: require('mysql2'),
+
+  }
+);
 
 module.exports = sequelize;
